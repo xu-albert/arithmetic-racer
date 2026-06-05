f981e13d6450783713156c0ccaf867420eed8802
Albert Xu <albertwxu@gmail.com>
2026-06-05 09:31:13 -0700

    Move getOrCreateDeviceId into identity.js

--- public/src/identity.js
+++ public/src/identity.js
@@ -1,5 +1,6 @@
 const KEY_ID = 'racerId';
 const KEY_HANDLE = 'racerHandle';
+const KEY_DEVICE = 'deviceId';
 
 export function getOrCreateRacerId() {
   let id = localStorage.getItem(KEY_ID);
@@ -18,4 +19,13 @@ export function setStoredHandle(handle) {
   if (typeof handle === 'string' && handle.length > 0) {
     localStorage.setItem(KEY_HANDLE, handle);
   }
+}
+
+export function getOrCreateDeviceId() {
+  let id = localStorage.getItem(KEY_DEVICE);
+  if (!id) {
+    id = crypto.randomUUID();
+    localStorage.setItem(KEY_DEVICE, id);
+  }
+  return id;
 }
